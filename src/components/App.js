02c86bd10c63d56dfe8aef145d50f1ac6d11fb71
ajak achiek <ajaksam39@gmail.com>
2025-11-03 04:07:15 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,17 @@
 import React from "react";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-{/* write an <About> component here */}
-
-
 function App() {
   return (
     <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <nav>
+        <a href="#home">Home</a>
+      </nav>
+      <div id="home">
+        <h1>Home</h1>
+      </div>
+      <div id="about">
+        <h2>About</h2>
+      </div>
     </div>
   );
 }
